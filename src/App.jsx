eb9c3bae922867950dcfab0fd9ff5c770318338b
balaji-aadi <balajiaadi2000@gmail.com>
2023-11-02 22:29:33 +0530

    Authentication

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Header from './Components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+       <h1>Mern Auth</h1>
+    </div>
+  )
+}
+
+export default App
